c8710c7e3bd77f172389eee23c4845ec1adeedf7
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-01-28 14:57:18 +0700

    update jadibot.js

--- shop/jadibot.js
+++ shop/jadibot.js
@@ -1,33 +1,43 @@
-const jadibut = () =>{
-return`╭──「 📖  *PRICE JADIBOT*」
-│
-├ • *Test Jadibot [30-60m]*
-│
-│ ⤷ *Free* 💌
-│
-├ • *Permanen*
-│
-│ ⤷ *Harga :  25k [E-Wallet]*
-│ ⤷ *Harga :  28k [Pulsa]*
-│
-├─ 「 💰  *PAYMENT* 」
-│
-│ ⤷ *Dana [Diutamakan]*
-│ ⤷ *Ovo*
-│ ⤷ *Gopay*
-│ ⤷ *Pulsa Telkom*
-│
-├─ 「 👨‍💻  *RUN ON* 」
-│
-│ ⤷ *Heroku [Diutamakan]*
-│ ⤷ *Railway* 
-│
-├─ 「 📝  *NOTES* 」
-│
-│ ⤷ _Tidak perlu termux_
-│ ⤷ _Minat? Hubungi 0887435047326_
-│
-╰──「 *Thank's* 」
-`
+
+let { WAConnection, MessageType, Mimetype} = require('@adiwajshing/baileys')
+let qrcode = require('qrcode')
+const fs = require('fs')
+
+listjadibot = [];
+
+const jadibot = async(reply,client,id) => {
+	conn = new WAConnection()
+    conn.logger.level = 'warn'
+    conn.version = [2, 2143, 3]
+    conn.browserDescription = [ 'By : DhaniGans', '', '3.0' ]
+    conn.on('qr', async qr => {
+    	let bot = await qrcode.toDataURL(qr, { scale: 8 })
+    	let buffer = new Buffer.from(bot.replace('data:image/png;base64,', ''), 'base64')
+       	bot = await client.sendMessage(id,buffer,MessageType.image,{caption:'Scan QR Untuk menjadi bot\n*Rules:*\nQR akan diganti setiap 30 detik'})
+    	setTimeout(() => {
+       	client.deleteMessage(id, bot.key)
+       },30000)
+    })
+    conn.on('connecting', () => {
+    })
+    conn.on('open', () => {
+    	reply(`Sukses Jadi BOT\n\n*Device*:\n\n ${JSON.stringify(conn.user,null,2)}`)
+    })
+    await conn.connect({timeoutMs: 30 * 1000})
+    listjadibot.push(conn.user)
+    conn.on('chat-update', async (message) => {
+        require('../index.js')(conn, message)
+    })
 }
-exports.jadibut = jadibut
+
+const stopjadibot = (reply) => {
+	conn = new WAConnection();
+	conn.close()
+	reply('Sukses stop jadibot')
+}
+
+exports.jadibut = {
+	jadibot,
+	stopjadibot,
+	listjadibot
+}
